f8647dbd995661ba9b2d0a6b6717b5076dd69114
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-09 13:50:13 +0300

    Add project files.

---
+++ NET-FRAMEWORK--NET-CORE/NETFrameworkCoreFarkları.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Principal;
+
+namespace NET_FRAMEWORK__NET_CORE
+{
+    public class NETFrameworkCoreFarkları
+    {
+        #region Farkları
+
+        // .NET Frameowrk => Sadece Windows ekosistemine yönelik çözümler getirir
+            
+        // .NET Core => Cross Platformdur. Çeşitli işletim sistemlerinde desteklenir.
+
+         
+        #endregion
+    }
+}
---
+++ ValueType-PrimitiveType/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+#region Değer Türlü değişkenler
+
+
+// Primitive type => ham veri, türetilmemiş, ilkel veri,
+
+// byte
+Console.WriteLine(typeof(decimal).IsPrimitive);
+Console.WriteLine(typeof(int).IsPrimitive);
+Console.WriteLine(typeof(byte).IsPrimitive);
+Console.WriteLine(typeof(double).IsPrimitive);
+
+// bu şekilde kontrol edilir
+#endregion

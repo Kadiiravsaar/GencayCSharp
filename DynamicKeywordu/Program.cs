987f1022395962000467bb6fb442fe8f5e18eb56
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-14 12:03:43 +0300

    ders 117 default değerler

--- DynamicKeywordu/Program.cs
+++ DynamicKeywordu/Program.cs
@@ -6,4 +6,18 @@ dynamic x = 15;
 
 // ne zaman ki uygulama derlenir çalıştırılır o  zaman ilgili türe bürünür  
 
-Ders 75 de kalındı
+// ********* Ders 117 ************
+
+// default değerler 
+
+// Sayısal => 0
+
+// bool => False
+
+// string => null
+
+// char => \0
+
+// referans => null
+
+    
